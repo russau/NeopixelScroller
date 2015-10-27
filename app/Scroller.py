268201418e9d07c2b5dbb8facf84d77d6d0c682a
russ <russell.sayers@gmail.com>
2015-10-27 12:00:53 +0000

    neopixel support

--- app/Scroller.py
+++ app/Scroller.py
@@ -1,6 +1,9 @@
 import time
 from random import randint
-
+try:
+    from neopixel import *
+except ImportError:
+    print "non-neopixel"
 
 class Scroller(object):
 
@@ -11,10 +14,15 @@ class Scroller(object):
         width = 6 * 10
         height = 8
         board = []
+
         for y in range(height):
+            #  add 'blankness' before the 'text'
             row = []
             for x in range(width):
-                color = {'r':0xe4, 'g':0xde, 'b':0x00} if (randint(0, 9) % 2) else {'r':0x00, 'g':0x4e, 'b':0xe4}
+                if x < 16:
+                    color = {'r':0x00, 'g':0x00, 'b':0x00}
+                else:
+                    color = {'r':0xe4, 'g':0xde, 'b':0x00} if (randint(0, 9) % 2) else {'r':0x00, 'g':0x4e, 'b':0xe4}
                 row.append(color)
             board.append(row)
 
@@ -52,3 +60,26 @@ def printBoard(board):
             print board[y][x],
         print ""
     print " ".join("=" for _ in range(width))
+
+def squaresEmitter(squares):
+    for i in range(strip.numPixels()):
+        color = squares[i]
+        strip.setPixelColorRGB(i, color['r'], color['g'], color['b']);
+    strip.show()
+
+if __name__ == '__main__':
+    # LED strip configuration:
+    LED_COUNT      = 128      # Number of LED pixels.
+    LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+    LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+    LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+    LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+    LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+    strip.begin()
+    strip.setBrightness(30)
+
+    while True:
+        s = Scroller(squaresEmitter)
+        s.scrollText()
